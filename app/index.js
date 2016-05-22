946048b902013cd01ac2304cdf5f73c41492c49e
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-05-22 19:11:27 +0200

    initial structure

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var generators = require('yeoman-generator');
+
+
+module.exports = generators.Base.extend({
+    constructor: function () {
+
+        generators.Base.apply(this, arguments);
+    })
+});
